59693e773bc067633cdb1a20525cd64c942e21f0
Murat Kutlu Tuna <muratkutlutuna@gmail.com>
2021-12-04 22:50:03 +0100

    1242021

--- Team/C01.java
+++ Team/C01.java
@@ -6,13 +6,61 @@ import java.util.List;
 public class C01 {
 
 	public static void main(String[] args) {
-		int arr[] = {3,5,1,2,7,9,2,3,5,7};
-		List<String>list = new ArrayList<>();
-		list.add(Integer.toString(arr[0]));
-		for (int i = 0; i < arr.length; i++) {
-			
+		 /* If the array is { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7 }
+         * type a program to create a new array whose elements are like {3, 5+1, 2+7+9, 2+3+5+7} {3, 6, 18, 17}
+         */
+		int arr[] = { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7, 4, 5, 6, 3, 9 };
+		
+
+		List<String> list = new ArrayList<String>();
+		int p = 0;
+		String c = "";
+		int s = 0;
+		for (int i = 0; i < arr.length && p < arr.length; i++) {
+			for (p = i; p <= i; p++) {
+				for (int j = s; j <= s + p && j < arr.length; j++) {
+					c += Integer.toString(arr[j])+"+";
+				}
+
+			}
+			c=c.substring(0,c.length()-1);
+			s += p;
+			if (c.equals("")) {
+				break;
+			}
+			list.add(c);
+			c = "";
+
 		}
 		
+		
+		
+		
+		//====================================================
+
+//		List<Integer> list = new ArrayList<>();
+//		int p = 0;
+//		int c = 0;
+//		int s = 0;
+//		for (int i = 0; i < arr.length && p < arr.length; i++) {
+//			for (p = i; p <= i; p++) {
+//				for (int j = s; j <= s + p && j < arr.length; j++) {
+//					c += arr[j];
+//					System.out.println(arr[j]);
+//					System.out.println("c=" + c);
+//				}
+//
+//			}
+//			s += p;
+//			if (c == 0) {
+//				break;
+//			}
+//			System.out.println();
+//			list.add(c);
+//			c = 0;
+//
+//		}
+
 		System.out.println(list);
 	}
 
